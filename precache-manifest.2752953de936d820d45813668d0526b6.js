cabf1c906cdf23e1576e8172c1bb96b2243a468d
Hemasree Duggina <hemaduggina123@gmail.com>
2022-10-08 18:40:34 +0530

    Updates

---
+++ precache-manifest.2752953de936d820d45813668d0526b6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ecd77a3e9d52d4abbf2",
+    "url": "/TekBitz/static/css/main.61d8e5f2.chunk.css"
+  },
+  {
+    "revision": "3ecd77a3e9d52d4abbf2",
+    "url": "/TekBitz/static/js/main.3ecd77a3.chunk.js"
+  },
+  {
+    "revision": "66b43ad12173adf7b342",
+    "url": "/TekBitz/static/js/1.66b43ad1.chunk.js"
+  },
+  {
+    "revision": "4b1cda7aa79a84960a95",
+    "url": "/TekBitz/static/js/2.4b1cda7a.chunk.js"
+  },
+  {
+    "revision": "e14b9c61ae8887425c2c",
+    "url": "/TekBitz/static/js/3.e14b9c61.chunk.js"
+  },
+  {
+    "revision": "4fe8f3b8afa301c7653a",
+    "url": "/TekBitz/static/css/4.e8005ffc.chunk.css"
+  },
+  {
+    "revision": "4fe8f3b8afa301c7653a",
+    "url": "/TekBitz/static/js/4.4fe8f3b8.chunk.js"
+  },
+  {
+    "revision": "ddcb7fd74aa0acd88541",
+    "url": "/TekBitz/static/js/runtime~main.ddcb7fd7.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TekBitz/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/TekBitz/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/TekBitz/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/TekBitz/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/TekBitz/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/TekBitz/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ab640a81b5022496444ecb841c7fb1c5",
+    "url": "/TekBitz/index.html"
+  }
+];
