a7b2ab7140144bc2d484923ab4838c3904d724cf
Hemasree Duggina <hemaduggina123@gmail.com>
2022-10-11 13:36:55 +0530

    third commit dependecies

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import Menu from "./Components/Menu";
 //import JSqstns from "./Components/JSqstns";
 //import Reactqstns from "./Components/Reactqstns";
-import Home from "./Components/Home";
+import Home  from "./Components/Home";
 import Suspense from "react";
 import { HashRouter, Switch, Route } from 'react-router-dom';
 const LazyJSqstns = React.lazy(() => import("./Components/JSqstns"))
@@ -13,24 +13,24 @@ const LazyReactqstns = React.lazy(() => import("./Components/Reactqstns"))
 export default function App() {
     return (
         <HashRouter>
-          <div className="App">
+            <div className="App">
                 <Menu />
-                  <Switch>
+                <Switch>
                     <Route exact path="/">
-                                  <Home />
-                              </Route>
-                            <Route path="/Javascriptquestions">
-                                <React.Suspense>
-                                    <LazyJSqstns />
-                                </React.Suspense>
-                              </Route>
-                            <Route path="/Reactquestions">  
-                                <React.Suspense>
-                                    <LazyReactqstns />
-                                </React.Suspense>
-                              </Route>
-                      </Switch>
-                </div>
+                        <Home />
+                    </Route>
+                    <Route path="/Javascriptquestions">
+                        <React.Suspense>
+                            <LazyJSqstns />
+                        </React.Suspense>
+                    </Route>
+                    <Route path="/Reactquestions">
+                        <React.Suspense>
+                            <LazyReactqstns />
+                        </React.Suspense>
+                    </Route>
+                </Switch>
+            </div>
         </HashRouter>
   );
 }
