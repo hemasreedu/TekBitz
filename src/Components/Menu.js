8a9e434807e22314c27c2956d41bfd648c7d4265
Hemasree Duggina <hemaduggina123@gmail.com>
2022-10-11 13:52:45 +0530

    remove import

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import 'font-awesome/css/font-awesome.min.css';
+//import 'font-awesome/css/font-awesome.min.css';
 
 export default function Menu() {
     const togglelinks = () => {
